a36387437c4a6ea5b69cc7a6d78c125a1c17eb7a
pkoky <sscsk2224@gmail.com>
2021-10-22 16:17:11 +0900

    Add sectionTemplate to HTML

--- main.js
+++ main.js
@@ -1,7 +1,58 @@
 
+class Section {
+    constructor(sectionName, id) {
+        this.sectionName = sectionName;
+        this.tasks = [];
+        this.id = id;
+    }
+}
+
+class Control {
+    static test() {
+        console.log('ok')
+    }
+
+    static createSectionObject(value) {
+        let obj = new Section(value);
+        return obj;
+    }
+}
+
+
+
+
+
+
+var sectionComponent = ({
+    props: ['section'],
+    data: function () {
+        return {
+          count: 0
+        }
+      },
+      methods: {
+          create: function() {
+              Control.createSectionObject();
+          }
+      },
+      template: '#sectionComponent',
+})
+
+
+
 new Vue({
     el: '#taskManagementApp',
     data: {
-        test: 'okay!!!'
+        newSectionTitle: '',
+        sections: [],
+    },
+    methods: {
+        createSection() {
+            this.sections.push(Control.createSectionObject(this.newSectionTitle));
+            this.newSectionTitle = '';
+        }
+    },
+    components: {
+        'section-component' : sectionComponent,
     }
 })
