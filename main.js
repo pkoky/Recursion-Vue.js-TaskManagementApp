5993655fcd2f3401e6c134ce94b574d258156962
pkoky <sscsk2224@gmail.com>
2021-10-22 20:59:16 +0900

    Add [createTaskForm] to Js]

--- main.js
+++ main.js
@@ -1,11 +1,24 @@
+class Task {
+    constructor(title, description) {
+        this.title = title;
+        this.description = description;
+        this.completed = false;
+        this.favorite = false;
+    }
+}
+
+
 
 class Section {
     constructor(sectionName, id) {
         this.sectionName = sectionName;
         this.tasks = [];
         this.id = id;
+        this.show = false;
     }
 }
+
+
 
 class Control {
     static test() {
@@ -16,26 +29,53 @@ class Control {
         let obj = new Section(value);
         return obj;
     }
+
+    static createTaskObject(title, description) {
+        return new Task(title, description);
+    }
 }
 
 
 
+var createTaskForm = ({
+    props: ['section'],
+    template: '#createTaskFormComponent',
+    data() {
+        return {
+            title: '',
+            description: '',
+        }
+    },
+    methods: {
+        addTask() {
+            this.section.show = false;
+            this.section.tasks.push(Control.createTaskObject(this.title, this.description));
+        }
+    }
+})
 
 
 
 var sectionComponent = ({
-    props: ['section'],
+    props: ['section','id'],
     data: function () {
         return {
           count: 0
         }
       },
+      created: function() {
+          this.section.id = this.id;
+      },
       methods: {
-          create: function() {
-              Control.createSectionObject();
-          }
+        switchTaskForm() {
+            this.section.show = this.section.show ? false : true;
+        }
       },
       template: '#sectionComponent',
+      components: {
+          'createTask-Form' : createTaskForm,
+
+      }
 })
 
 
